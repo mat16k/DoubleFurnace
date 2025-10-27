57ecc031dd6dce0b8fd93eca2561686f2616179d
Lennart Buhl <git@hackmate.de>
2025-10-27 15:12:32 +0100

    keep formatting more in line with style from base mod

--- settings.lua
+++ settings.lua
@@ -1,8 +1,10 @@
-data:extend({
-    {
-        type = "bool-setting",
-        name = "doublefurnace-research",
-        setting_type = "startup",
-        default_value = true
-    }
-})
+data:extend(
+{
+  {
+    type = "bool-setting",
+    name = "doublefurnace-research",
+    setting_type = "startup",
+    default_value = true
+  }
+}
+)
